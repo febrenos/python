a5012e1508ee65e883547c0dc79e76754345bd45
Fe <54954812+febreno@users.noreply.github.com>
2022-11-03 23:41:07 -0300

    Update selectionSort.py

--- Content/selectionSort.py
+++ Content/selectionSort.py
@@ -7,12 +7,11 @@ def  menor(lst, i):
         i = i + 1
     return pos_menor
 
-conj = [2,-7,9,3,,10,15,6]
+conj = [2,-7,9,3,10,15,6]
 
-for i in range(len(conj))
-j = menor(conj, i)
-aux = conj[j]
-conj[j] = conj[i]
-conj[i] = aux
-
+for i in range(len(conj)):
+    j = menor(conj, i)
+    aux = conj[j]
+    conj[j] = conj[i]
+    conj[i] = aux
 print(conj)
